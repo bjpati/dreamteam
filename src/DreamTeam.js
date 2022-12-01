c53364b159832f52014bb27857923afb3b3bbd4f
Hasan Alzubeidi <97052700+HasanAlzubeidi@users.noreply.github.com>
2022-12-01 18:06:41 +0100

    Create TeamPlayer first-version

--- src/DreamTeam.js
+++ src/DreamTeam.js
@@ -1,21 +1,75 @@
 /* eslint-disable */
 class DreamTeam {
-  constructor() {
+  constructor(currentPlayerId) {
     this.observers = []
-    this.players = []
+    const player = {id:"6cb7dbe26a2d56b",
+    name:"Nicolas Colazo",
+    country:"AR",
+    birth_date:"1990-07-08",
+    foot:"Left",
+    height:"175 cm",
+    has_image:false};
+
+    const player1 = {id:"4ab7dbe26a2d56b",
+    name:"Lionell messi",
+    country:"AR",
+    birth_date:"1993-03-08",
+    foot:"right",
+    height:"170 cm",
+    has_image:true}
+
+
+
+
+    this.players = [player, player1];
   }
 
   addToTeam(playerToAdd) {
-    if (this.players.find(playerToAdd)) {
-      return
+    if (this.players.find(findPlayerCB)) {
+      return;
     }
 
-    this.playerToAdd = [...this.players, playerToAdd]
+    this.players= [...this.players, playerToAdd]
 
-    function findPlayerCB(dish) {
+    function findPlayerCB(player) {
       return player.id === playerToAdd.id
     }
   }
+
+
+  removePlayer(playerToRemove){
+
+    function hasSameIdCB(player) {
+    
+      if (player.id !== playerToRemove.id)
+          return true;
+      else
+          return false;
+  }
+
+  if(this.players.find(findPlayerCB)){
+    this.players = this.players.filter(hasSameIdCB);
+  
+}
+
+
+function findPlayerCB(player) {
+  return player.id === playerToAdd.id;
+}
+
+
+  }
+
+
+  setCurrentPlayer(id){
+
+    if(id && id!==this.currentPlayerId){
+      this.currentPlayerId=id;
+    }
+  }
+
+
+
 }
 
 export default DreamTeam
